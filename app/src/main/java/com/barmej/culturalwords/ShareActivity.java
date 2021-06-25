e86955ec9a17696d84f8cd7e4d1415e7a02ff092
Mohammed Mansour <shalia.kwt@outlook.com>
2021-06-25 23:15:26 +0300

    LAST VERSION

--- app/src/main/java/com/barmej/culturalwords/ShareActivity.java
+++ app/src/main/java/com/barmej/culturalwords/ShareActivity.java
@@ -9,15 +9,26 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ShareActivity extends AppCompatActivity {
 
     private static final int PERMISSIONS_WRITE_EXTERNAL_STORAGE = 123;
@@ -39,7 +50,16 @@ public class ShareActivity extends AppCompatActivity {
         String questionTitle = sharedPreferences.getString(SHARED_PREFERENCES_TITLE, "");
         sharedTitle.setText(questionTitle);
     }
-    public void checkPermissionAndShare(View view)
+
+    public void androidVersion(View view){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+            backgroundTask backgroundTask = new backgroundTask();
+            backgroundTask.start();
+        }else{
+            checkPermissionAndShare();
+        }
+    }
+    public void checkPermissionAndShare()
     {
         // insertImage في النسخ من آندرويد 6 إلى آندرويد 9 يجب طلب الصلاحية لكي نتمكن من استخدام الدالة
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
@@ -49,7 +69,8 @@ public class ShareActivity extends AppCompatActivity {
             {
                 // بسبب عدم منح المستخدم الصلاحية للتطبيق فمن الأفضل شرح فائدتها له عن طريق عرض رسالة تشرح ذلك
                 // هنا نقوم بإنشاء AlertDialog لعرض رسالة تشرح للمستخدم فائدة منح الصلاحية
-                AlertDialog alertDialog = new AlertDialog.Builder(this).setTitle(R.string.permission_title).setMessage(R.string.permission_explanation).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {@Override
+                AlertDialog alertDialog = new AlertDialog.Builder(this).setTitle(R.string.permission_title).setMessage(R.string.permission_explanation).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     // requestPermissions عند الضغط على زر منح نقوم بطلب الصلاحية عن طريق الدالة
                     ActivityCompat.requestPermissions(ShareActivity.this, new String[] {
@@ -59,12 +80,12 @@ public class ShareActivity extends AppCompatActivity {
                 }
                 }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener()
                 {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i)
-                {
-                    //  عند الضغط على زر منع نقوم بإخفاء الرسالة وكأن شيء لم يكن
-                    dialogInterface.dismiss();
-                }
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i)
+                    {
+                        //  عند الضغط على زر منع نقوم بإخفاء الرسالة وكأن شيء لم يكن
+                        dialogInterface.dismiss();
+                    }
                 }).create();
                 // نقوم بإظهار الرسالة بعد إنشاء alertDialog
                 alertDialog.show();
@@ -76,6 +97,7 @@ public class ShareActivity extends AppCompatActivity {
                                 Manifest.permission.WRITE_EXTERNAL_STORAGE
                         },
                         PERMISSIONS_WRITE_EXTERNAL_STORAGE);
+
             }
 
         }
@@ -83,6 +105,22 @@ public class ShareActivity extends AppCompatActivity {
         {
             // الصلاحية ممنوحه مسبقا لذلك يمكننا مشاركة الصورة
             shareButton();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                shareButton();
+
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.permission_declined, Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
 
@@ -117,4 +155,32 @@ public class ShareActivity extends AppCompatActivity {
 
         return imageUri;
     }
+
+
+    class backgroundTask extends Thread
+    {
+        @Override
+        public void run() {
+
+                try {
+                    // putting it into a file since 24+API doesn't allow to get Uri out of the application.
+                    File photosDir = new File(getCacheDir() , "photos");
+                    if (!photosDir.exists()){
+                        photosDir.mkdirs();
+                    }
+                    File file = new File(photosDir, System.currentTimeMillis() + "." + "png");
+                    FileOutputStream fOut = new FileOutputStream(file);
+                    fOut.flush();
+                    fOut.close();
+                    shareButton();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+    }
+
 }
